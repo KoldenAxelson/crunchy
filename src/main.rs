605deddf53292dc14ed2dd8aa2449e602155b9d7
KoldenAxelson <KoldenAxelson896@Protonmail.com>
2025-09-01 13:04:53 -0700

    Changed to do dub_offset

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     // Can't Post my Login Credentials bc of Degens on the Internet
     let read_to_string = fs::read_to_string("/Library/.my_credentials")?;
     let credentials_file = read_to_string;
-    let credentials : Vec<&str> = credentials_file.rsplit(",").collect();
+    let credentials: Vec<&str> = credentials_file.rsplit(",").collect();
 
     let crunchyroll = Crunchyroll::builder()
         .login_with_credentials(credentials[1], credentials[0])
@@ -18,12 +17,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // println!("{:?}", token);
 
     // Get Seasons
-    // let a: Series = crunchyroll.media_from_id("GRMG8ZQZR").await?;
+    // let a: Series = crunchyroll.media_from_id("GG5H5XQG5").await?;
     // let b = a.seasons().await?;
     // for c in b {
-    //     println!("{:?} Season {:?} | {:?}",c.title,c.season_number,c.id);
+    //     println!("{:?} Season {:?} | {:?}", c.title, c.season_number, c.id);
     //     let d = c.versions;
-    //     for e in d {println!("{:?} {:?}", e.id, e.audio_locale);}
+    //     for e in d {
+    //         println!("{:?} {:?}", e.id, e.audio_locale);
+    //     }
     // }
 
     // Episode Details from Season
@@ -48,32 +49,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut shows: Vec<Vec<String>> = Vec::new();
 
     // "GRMG8ZQZR" One Piece
-    shows.push(get_info(crunchyroll.clone(), "GYP8PM4KY", "", false).await?);
-
-    // "G6NQ5DWZ6" My Hero Academia
-    shows.push(get_info(crunchyroll.clone(), "G6DQCG592", "My Hero", true).await?);
-
-    // "G4PH0WEKE" BLUE LOCK
-    shows.push(get_info(crunchyroll.clone(), "G675CD9NG", "BLUE LOCK", true).await?);
-
-    // "G6J0G49DR" Tower of God  
-    shows.push(get_info(crunchyroll.clone(), "G69PC28Q1", "Tower God", true).await?);
+    shows.push(get_info(crunchyroll.clone(), "GYP8PM4KY", "", 0).await?);
 
     // "GG5H5XQ0D" DAN DA DAN
-    shows.push(get_info(crunchyroll.clone(), "GYJQC1VNW", "DAN DA DAN", true).await?);
+    shows.push(get_info(crunchyroll.clone(), "G6NQCJVPD", "DAN DA DAN", 0).await?);
+
+    // "GYEXQKJG6" DR STONE
+    shows.push(get_info(crunchyroll.clone(), "GR19CPMDD", "DR STONE", 0).await?);
+
+    // "GG5H5XQ7D" Kaiju No. 8
+    shows.push(get_info(crunchyroll.clone(), "G65VCD19G", "Kaiju #8", 0).await?);
+
+    // "GQWH0M1J3" To Be Hero X
+    shows.push(get_info(crunchyroll.clone(), "GRJQC1EN0", "2B Hero X", 0).await?);
+
+    // "G9VHN9Q3G" Secrets of the Silent Witch
+    shows.push(get_info(crunchyroll.clone(), "GR19CPGG5", "Silent Witch", 0).await?);
+
+    // "GG5H5XQG5" The Water Magician
+    shows.push(get_info(crunchyroll.clone(), "GY19CPGM0", "Water Mage", 2).await?);
 
     // Hiatus
     // "GEXH3WKK0" Vinland Sage
     // "GDKHZEP8W" MASHLE: MAGIC AND MUSCLES
     // "GVDHX8Q71" Why Raeliana Ended Up at the Duke's Mansion
     // "GNVHKNPQ7" My Love Story with Yamada-kun at Lv999
-    // "GYEXQKJG6" DR STONE
     // "GY5P48XEY" Demon Slayer: Kimetsu no Yaiba
-    // "G0XHWM0D3" Trapped in a Dating Sim! 
+    // "G0XHWM0D3" Trapped in a Dating Sim!
     // "GVDHX8504" Reborn as a Vending Machine
     // "GEXH3W2Z7" Reign of the Seven Spellblades
     // "G9VHN9P43" Horimiya: Missing Piecies
-    // "G5PHNM7E2" Sugar Apple Fairy Tale 
+    // "G5PHNM7E2" Sugar Apple Fairy Tale
     // "G0XHWM52V" Sacrificial Princess and the King of Beasts
     // "GEXH3W2V7" Sign of Affection
     // "GDKHZEJ0K" Solo Leveling
@@ -82,86 +88,101 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // "GG5H5XQX4" Frieren: Beyond Journey's End
     // "GYE5K3GQR" Konosuba
     // "GJ0H7QGME" Viral Hit
-    // "G3KHEVDPE" Wind Breaker 
-    // "GG5H5XQ7D" Kaiju No. 8 
+    // "G3KHEVDPE" Wind Breaker
     // "G24H1N3MP" Mushoku Tensei: Jobless Reincarnation
-    // "GY243NN0R" The Misfit of Demon King Academy 
-    // "GP5HJ80VJ" How Heavy Are the Dumbbells You Lift? 
-    // "GW4HM7WK9" Wistoria: Wand and Sword 
+    // "GY243NN0R" The Misfit of Demon King Academy
+    // "GP5HJ80VJ" How Heavy Are the Dumbbells You Lift?
+    // "GW4HM7WK9" Wistoria: Wand and Sword
     // "GDKHZEJ20" VTuber Legend: How I Went Viral after Forgetting to Turn Off My Stream
     // "G1XHJV0XM" Alya Sometimes Hides Her Feelings in Russian
     // "GYZJ43JMR" That Time I Got Reincarnated as a Slime
     // "G79H23Z8P" Shangri-La Frontier !!!
+    // "G6NQ5DWZ6" My Hero Academia
+    // "G4PH0WEKE" BLUE LOCK
+    // "G6J0G49DR" Tower of God
 
     shows.sort_by_key(|show| show[0].parse::<u64>().unwrap());
 
     for show in shows {
         let show_check: bool = show[3].parse().unwrap();
-        if !show_check {continue;}
-        println!("{}{}{}",show[2],show[1],"\x1b[0m");
+        if !show_check {
+            continue;
+        }
+        println!("{}{}{}", show[2], show[1], "\x1b[0m");
     }
 
     Ok(())
 }
 
-async fn get_info(cr: Crunchyroll, season_id: &str, alt_title: &str, dub: bool) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+async fn get_info(
+    cr: Crunchyroll,
+    season_id: &str,
+    alt_title: &str,
+    dub_offset: i64,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let season: Season = cr.media_from_id(season_id).await?;
     let episodes = season.episodes().await?;
-    let mut episode: &Episode = &episodes[episodes.len()-1];
-    if dub {
-        for n in 0..episodes.len()-1 {
-            if format!("{:?}",episodes[n].audio_locale).eq("en_US") || episodes[n].is_dubbed {
+    let mut episode: &Episode = &episodes[episodes.len() - 1];
+    if dub_offset != 0 {
+        for n in 0..episodes.len() - 1 {
+            if format!("{:?}", episodes[n].audio_locale).eq("en_US") || episodes[n].is_dubbed {
                 episode = &episodes[n];
             }
         }
     }
-
-    let now       = std::time::SystemTime::now();
-    let sec: i64  = (now.duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() * 1000) as i64;
+    let dub: bool = dub_offset != 0;
+    let now = std::time::SystemTime::now();
+    let sec: i64 = (now.duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() * 1000) as i64;
     let mut time: i64 = sec - (episode.premium_available_date.timestamp_millis() as i64);
-    if dub && !format!("{:?}",episode.audio_locale).eq("en_US") {time -= 604_800_000 * 2;}
+    if dub && !format!("{:?}", episode.audio_locale).eq("en_US") {
+        time -= 604_800_000 * dub_offset;
+    }
 
     let mut info_string: String = "".to_string();
 
     // Time Until (Estimate)
-    let remaining_days  =  (604_800_000 - time) / 86_400_000;
+    let remaining_days = (604_800_000 - time) / 86_400_000;
     let remaining_hours = ((604_800_000 - time) % 86_400_000) / 3_600_000;
-    let remaining_mins  = ((604_800_000 - time) %  3_600_000) /    60_000;
+    let remaining_mins = ((604_800_000 - time) % 3_600_000) / 60_000;
     if remaining_days <= 7 && (604_800_000 - time) >= 0 {
-        info_string.push_str(
-            &format!("-{:0>2}:{:0>2}:{:0>2} ",
-                remaining_days,
-                remaining_hours,
-                remaining_mins));
+        info_string.push_str(&format!(
+            "-{:0>2}:{:0>2}:{:0>2} ",
+            remaining_days, remaining_hours, remaining_mins
+        ));
     } else {
-        let time_over     = time - 604_800_000;
-        let elapsed_days  = time_over / 86_400_000;
+        let time_over = time - 604_800_000;
+        let elapsed_days = time_over / 86_400_000;
         let elapsed_hours = time_over % 86_400_000 / 3_600_000;
-        let elapsed_mins  = time_over %  3_600_000 /    60_000;
-        info_string.push_str(
-            &format!("+{:0>2}:{:0>2}:{:0>2} ",
-                elapsed_days,
-                elapsed_hours,
-                elapsed_mins));
+        let elapsed_mins = time_over % 3_600_000 / 60_000;
+        info_string.push_str(&format!(
+            "+{:0>2}:{:0>2}:{:0>2} ",
+            elapsed_days, elapsed_hours, elapsed_mins
+        ));
     }
 
-    info_string.push_str(&format!("{} ",episode.premium_available_date.format("%a")));
+    info_string.push_str(&format!("{} ", episode.premium_available_date.format("%a")));
 
     if alt_title == "" {
-        info_string.push_str(&format!("{: <15}",episode.series_title));
+        info_string.push_str(&format!("{: <15}", episode.series_title));
     } else {
-        info_string.push_str(&format!("{: <15}",alt_title));
+        info_string.push_str(&format!("{: <15}", alt_title));
     }
     info_string.push_str("  ");
 
-    info_string.push_str(&format!("S{:0>2}",episode.season_number));
-    info_string.push_str(&format!("E{:0>4}",episode.sequence_number));
+    info_string.push_str(&format!("S{:0>2}", episode.season_number));
+    info_string.push_str(&format!("E{:0>4}", episode.sequence_number));
     info_string.push_str("  ");
 
-    info_string.push_str(&format!("{}", match episode.is_dubbed {true => "EN", false => "JP"}));
+    info_string.push_str(&format!(
+        "{}",
+        match episode.is_dubbed {
+            true => "EN",
+            false => "JP",
+        }
+    ));
     info_string.push_str("  ");
 
-    info_string.push_str(&format!("{:?}",episode.title));
+    info_string.push_str(&format!("{:?}", episode.title));
 
     let color: String = match time / 86_400_000 {
         0 => "\x1b[38;5;2m".to_string(),    // Green
@@ -174,10 +195,10 @@ async fn get_info(cr: Crunchyroll, season_id: &str, alt_title: &str, dub: bool) 
         7 => "\x1b[38;5;14m".to_string(),   // Cyan
         8 => "\x1b[38;5;240m".to_string(),  // Grey
         9 => "\x1b[38;5;240m".to_string(),  // Grey
-        10=> "\x1b[38;5;240m".to_string(),  // Grey
-        11=> "\x1b[38;5;240m".to_string(),  // Grey
-        12=> "\x1b[38;5;240m".to_string(),  // Grey
-        13=> "\x1b[38;5;240m".to_string(),  // Grey
+        10 => "\x1b[38;5;240m".to_string(), // Grey
+        11 => "\x1b[38;5;240m".to_string(), // Grey
+        12 => "\x1b[38;5;240m".to_string(), // Grey
+        13 => "\x1b[38;5;240m".to_string(), // Grey
         _ => "\x1b[38;5;160m".to_string(),  // Red
     };
 
@@ -185,7 +206,7 @@ async fn get_info(cr: Crunchyroll, season_id: &str, alt_title: &str, dub: bool) 
 
     let dub_matched = (dub && episode.is_dubbed) || !dub;
     let is_expired = remaining_days < -30;
-    let will_show = format!("{:?}",dub_matched && !is_expired);
+    let will_show = format!("{:?}", dub_matched && !is_expired);
 
     let return_vec: Vec<String> = vec![time_string, info_string, color, will_show];
 
